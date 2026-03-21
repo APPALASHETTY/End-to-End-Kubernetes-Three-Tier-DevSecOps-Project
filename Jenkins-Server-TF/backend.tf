ad1c7823be74268d055e1f69a82eb1324aa6264d
APPALASHETTY <110849354+APPALASHETTY@users.noreply.github.com>
2026-03-22 00:17:20 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "mys3-sravs-bucket-12345"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "MySampleTable"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
   required_version = ">= 1.9.3"
